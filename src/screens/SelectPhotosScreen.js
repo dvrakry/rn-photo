a6d464d2220cbbfc01ba68fdec409e0f27bad3fe
Roy Kang <dv.rakry@gmail.com>
2025-03-20 17:10:15 +0900

    10.4 글 작성 화면에서 사진이 출력되지 않는 문제 발생.

--- src/screens/SelectPhotosScreen.js
+++ src/screens/SelectPhotosScreen.js
@@ -1,17 +1,95 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
-import PropTypes from 'prop-types';
-import { useNavigation } from '@react-navigation/native';
+import {
+  Alert,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { MainRoutes } from '../navigations/routes';
+import { GRAY } from '../colors';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import HeaderRight from '../components/HeaderRight';
+import { getLocalUri } from '../components/ImagePicker';
+import ImageSwiper from '../components/ImageSwiper';
 
 const SelectPhotosScreen = () => {
   const navigation = useNavigation();
+  const { params } = useRoute();
+  const width = useWindowDimensions().width;
+
+  const [photos, setPhotos] = useState([]);
+  const [disabled, setDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setDisabled(isLoading || !photos.length);
+  }, [isLoading, photos.length]);
+
+  useEffect(() => {
+    if (params) {
+      const { selectedPhotos } = params;
+      if (selectedPhotos?.length) {
+        setPhotos(selectedPhotos);
+      }
+    }
+  }, [params]);
+
+  const onCofirm = useCallback(async () => {
+    if (!disabled) {
+      setIsLoading(true);
+      try {
+        const photoUris = await Promise.all(
+          photos.map((photo) =>
+            Platform.select({
+              ios: getLocalUri(photo.id),
+              android: photo.uri,
+            })
+          )
+        );
+        navigation.navigate(MainRoutes.WRITE_TEXT, { photoUris });
+      } catch (e) {
+        Alert.alert('사진 정보 조회 실패', e.message);
+      }
+      setIsLoading(false);
+    }
+  }, [disabled, photos]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <HeaderRight disabled={disabled} onPress={onCofirm} />,
+    });
+  }, [disabled, navigation, onCofirm]);
+
+  console.log('photos', photos);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>SelectPhotosScreen</Text>
-      <Button
-        title={'tab'}
-        onPress={() => navigation.navigate(MainRoutes.CONTETN_TAB)}
-      />
+      <Text style={styles.description}>
+        이미지는 최대 4장까지 선택 가능합니다.
+      </Text>
+
+      <View style={{ width, height: width }}>
+        {photos.length ? (
+          <ImageSwiper photos={photos} />
+        ) : (
+          <Pressable
+            onPress={() =>
+              navigation.navigate(MainRoutes.IMAGE_PICKER, { maxCount: 4 })
+            }
+            style={styles.photoButton}
+          >
+            <MaterialCommunityIcons
+              name="image-plus"
+              size={80}
+              color={GRAY.DEFAULT}
+            />
+          </Pressable>
+        )}
+      </View>
     </View>
   );
 };
@@ -19,11 +97,18 @@ const SelectPhotosScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  description: {
+    color: GRAY.DARK,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  photoButton: {
+    backgroundColor: GRAY.LIGHT,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  title: {
-    fontSize: 30,
   },
 });
 
